c90754a6f0217902e975db240e2626174ed0725e
caiobasilio <caiobasilio@Caios-MacBook-Pro.local>
2024-09-21 11:33:24 -0300

    Incluindo exercicios List, sobre Operacoes Basicas.

---
+++ com/dio/collections/list/operacoesbasicas/ListaTarefa.java
@@ -0,0 +1,62 @@
+package com.dio.collections.list.operacoesbasicas;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ListaTarefa {
+
+    private List<Tarefa> tarefaList;
+
+    public ListaTarefa() {
+        this.tarefaList = new ArrayList<>();
+    }
+
+    public void adicionarTarefa(String descricao) {
+        tarefaList.add(new Tarefa(descricao));
+    }
+
+    public void removerTarefa (String descricao) {
+        List<Tarefa> tarefasParaRemover = new ArrayList<>();
+        for (Tarefa t : tarefaList) {
+            if (t.getDescricao().equalsIgnoreCase(descricao)) {
+                tarefasParaRemover.add(t);
+            }
+        }
+        tarefaList.removeAll(tarefasParaRemover);
+    }
+
+    public int obterNumeroTotalTarefas() {
+        return tarefaList.size();
+    }
+
+    public List<String> obterDescricoesTarefas() {
+        List<String> descricoesDaListaDeTarefas = new ArrayList<String>();
+        if (!tarefaList.isEmpty()) {
+            for (Tarefa t : tarefaList) {
+                descricoesDaListaDeTarefas.add(t.getDescricao());
+            }
+        }
+        return descricoesDaListaDeTarefas;
+    }
+
+    public static void main(String[] args) {
+        ListaTarefa listaTarefa = new ListaTarefa();
+
+        System.out.println("O número total de tarefas é: " + listaTarefa.obterNumeroTotalTarefas());
+
+
+        listaTarefa.adicionarTarefa("Tarefa 1");
+        listaTarefa.adicionarTarefa("Tarefa 2");
+        listaTarefa.adicionarTarefa("Tarefa 3");
+        System.out.println("O número total de tarefas é: " + listaTarefa.obterDescricoesTarefas());;
+        
+        System.out.println("O número total de tarefas é: " + listaTarefa.obterNumeroTotalTarefas());
+        listaTarefa.removerTarefa("Tarefa 1");
+        System.out.println("O número total de tarefas é: " + listaTarefa.obterDescricoesTarefas());;
+
+        System.out.println("O número total de tarefas é: " + listaTarefa.obterNumeroTotalTarefas());
+
+
+
+    }
+}
